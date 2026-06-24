a44c039cb2a3b41253d49eb3ad99ba7f57441943
Cheffish Goyal <cheffishg3094@gmail.com>
2026-06-24 15:18:25 +0530

    Initial Commit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,29 +6,31 @@ import "./Home.css";
 
 export default function Home() {
   return (
-    <div className="home-container">
+    <>
       <Navbar />
-      <h1>Welcome to Invoice Generator</h1>
-      <p>Create beautiful invoices in seconds.</p>
-      <Link to="/signup">
-        <button>Sign Up</button>
-      </Link>
-      <Link to="/login">
-        <button>Login</button>
-      </Link>
-      <Link to="/verify">
-        <button>Verify OTP</button>
-      </Link>
-      <Link to="/forgotpassword">
-        <button>Forgot Password</button>
-      </Link>
-      <Link to="/resetpassword">
-        <button>Reset Password</button>
-      </Link>
-      <Link to="/logout">
-        <button>Logout</button>
-      </Link>
+      <div className="home-container">
+        <h1>Welcome to Invoice Generator</h1>
+        <p>Create beautiful invoices in seconds.</p>
+        <Link to="/signup">
+          <button>Sign Up</button>
+        </Link>
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
+        <Link to="/verify">
+          <button>Verify OTP</button>
+        </Link>
+        <Link to="/forgotpassword">
+          <button>Forgot Password</button>
+        </Link>
+        <Link to="/resetpassword">
+          <button>Reset Password</button>
+        </Link>
+        <Link to="/logout">
+          <button>Logout</button>
+        </Link>
+      </div>
       <Footer />
-    </div>
+    </>
   );
 }
